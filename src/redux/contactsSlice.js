0524a732594b37e9d911e03f7a36c3fd40e21d99
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-11-04 13:39:47 +0200

    fix2

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,7 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  contacts: [],
+  contacts: [
+    {
+      id: '1',
+      name: 'Ivan Petrenko',
+      number: '099-122-32-20',
+    },
+    {
+      id: '2',
+      name: 'Ivan Babich',
+      number: '099-122-32-23',
+    },
+    {
+      id: '3',
+      name: 'Ivan Kobzar',
+      number: '099-122-32-22',
+    },
+    {
+      id: '4',
+      name: 'Ivan Roberto',
+      number: '099-122-32-21',
+    },
+  ],
   filter: '',
 };
 
